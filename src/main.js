7c21e08cd1653df7d248d68af28c39b46523d81a
Quinn Miller and Rohan Moore <rohan@>
2019-01-15 14:52:56 -0800

    add loop to get ALL images from tthe mars rover curiosity

--- src/main.js
+++ src/main.js
@@ -2,14 +2,17 @@ import { Nasa } from './nasa.js'
 import $ from 'jquery'
 
 $(document).ready(function() {
-  let nasa = new Nasa();
-  let promise = nasa.getMarsImage($("#user-input").val());
-  promise.then(function(response) {
-    let body = JSON.parse(response);
-    body.photos.forEach(function(image){
-      $("#nasa").append(`<img src="${image.img_src}">`)
+  for(let i = 1; i <= 1000; i++) {
+    $("#counter").text(i);
+    let nasa = new Nasa();
+    let promise = nasa.getMarsImage(i);
+    promise.then(function(response) {
+      let body = JSON.parse(response);
+      body.photos.forEach(function(image){
+        $("#nasa").append(`<img src="${image.img_src}">`)
+      });
+    }, function(error) {
+      $("#error").text(`${error.message}`)
     });
-  }, function(error) {
-    $("#error").text(`${error.message}`)
-  });
+  }
 })
--- src/nasa.js
+++ src/nasa.js
@@ -1,9 +1,9 @@
 export class Nasa {
 
-  getMarsImage(image) {
+  getMarsImage(day) {
     return new Promise(function(resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=${process.env.API_KEY}`
+      let url = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=${day}&api_key=${process.env.API_KEY}`;
       request.onload = function() {
         if(this.status === 200) {
           resolve(request.response);
